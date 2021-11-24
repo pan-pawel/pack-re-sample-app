d8ab731054b9a1cdae76dc967cadbcc933a58ab8
Pawel Szymanski <3x4to9@gmail.com>
2021-11-24 14:12:22 +0100

    build

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
  * @format
  */
 
-import { AppRegistry } from "react-native";
+import { AppRegistry, Platform } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
+import ChunkManager from '@callstack/repack/client'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
+ChunkManager.configure({
+    storage: AsyncStorage, // optional
+    resolveRemoteChunk: async (chunkId) => {
+      // Feel free to use any kind of remote config solution to obtain
+      // a base URL for the chunks, if you don't know where they will
+      // be hosted.
+  
+      return {
+        url: `https://raw.githubusercontent.com/pan-pawel/pack-re-sample-app/master/chunks/${Platform.OS}/${chunkId}`,
+      };
+    },
+  });
+  
 AppRegistry.registerComponent(appName, () => App);

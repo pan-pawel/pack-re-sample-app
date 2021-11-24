99cb86e4520047701b20fd57752bac7e340e09ed
Pawel Szymanski <3x4to9@gmail.com>
2021-11-24 18:16:56 +0100

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import { AppRegistry, Platform } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-import ChunkManager from '@callstack/repack/client'
+import { ChunkManager } from '@callstack/repack/client'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
